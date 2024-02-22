f650a95d9cc1ee842b4f2fa67a324e38145985ac
Ukeme David Eseme <skywalkershadow9@gmail.com>
2024-02-22 15:53:38 +0000

    send mail - node status

--- network_monitor.sh
+++ network_monitor.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # Log file path
-LOG_FILE="network_monitor.log"
+logs_folder="./logs/"
+
+# Check if the logs folder exists
+if [ ! -d "$logs_folder" ]; then
+    echo "Logs folder does not exist. Creating..."
+    mkdir -p "$logs_folder"
+    echo "Logs folder created successfully."
+else
+    echo "Logs folder already exists."
+fi
+
+LOG_FILE="./logs/network_monitor.log"
+
+
+# Email configuration
+recipient="ukemzyreloaded@gmail.com"
+subject="Network Monitor Alert: Node Unreachable"
 
 # Ping a list of specified nodes and display the status of each node (reachable or unreachable).
 # Check if input is provided
@@ -17,7 +33,38 @@ log_status() {
     node=$1
     status=$2
     echo "$timestamp - $node is $status" >> "$LOG_FILE"
+
+    # Check if status is unreachable and send email notification
+    if [ "$status" = "unreachable" ]; then
+        send_email "$node"
+    fi
 }
+
+# Function to send email notification
+send_email() {
+    node=$1
+    message="Node $node is unreachable at $(date '+%Y-%m-%d %H:%M:%S')"
+    
+# JSON payload for the API request
+
+    payload=$(cat <<EOF
+        {
+        "from": "Guy <onboarding@resend.dev>",
+        "to": ["$recipient"],
+        "subject": "$subject",
+        "text": "$message"
+        }
+EOF
+    )
+
+     # Send the email using curl
+    curl -X POST 'https://api.resend.com/emails' \
+     -H 'Authorization: Bearer re_TP1WuRGm_3Moc3sM6atmk9Aip1ufaWWko' \
+     -H 'Content-Type: application/json' \
+     -d "$payload"
+
+}
+
 
 # Loop through each argument (node)
 for node in "$@"; do
